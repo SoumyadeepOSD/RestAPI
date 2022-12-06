5868534c4f975402e4751d5293fdb55cae451943
SoumyadeepOSD <opencontainer255@gmail.com>
2022-12-06 17:09:58 +0530

    Files Added

---
+++ data/users.js
@@ -0,0 +1,137 @@
+const users = 
+[
+    {
+        "id": 1,
+        "name" : "Andrew-Derek",
+        "location" : "USA",
+        "age" : 19
+    },
+    {
+        "id" : 2,
+        "name" : "Nikita-petrakov",
+        "location" : "Russia",
+        "age" : 22
+    },  
+    {
+        "id" : 3,
+        "name" : "Benjamin-Franklin",
+        "location" : "German",
+        "age" : 19
+    },
+    {
+        "id" : 4,
+        "name" : "John Graham",
+        "location" : "UK",
+        "age" : 23
+    },  
+    {
+        "id" : 5,
+        "name" : "Mike-Holand",
+        "location" : "UK",
+        "age" : 27
+    },
+    {
+        "id" : 6,
+        "name" : "John-Price",
+        "location" : "UK",
+        "age" : 56
+    },  
+    {
+        "id" : 7,
+        "name" : "Vladimir-Makarov",
+        "location" : "Makarov",
+        "age" : 29
+    },
+    {
+        "id" : 8,
+        "name" : "Peter-Johan",
+        "location" : "Germany",
+        "age" : 22
+    },  
+    {
+        "id" : 9,
+        "name" : "Andrew-Mosley",
+        "location" : "North-Amarica",
+        "age" : 24
+    },
+    {
+        "id" : 10,
+        "name" : "Derik-o-Brain",
+        "location" : "UAE",
+        "age" : 26
+    },  
+    {
+        "id" : 11,
+        "name" : "Misha-Petranova",
+        "location" : "Russia",
+        "age" : 20
+    },
+    {
+        "id" : 12,
+        "name" : "Nike-Merit",
+        "location" : "Itly",
+        "age" : 26
+    },  
+    {
+        "id" : 13,
+        "name" : "Soumaydeep Das Bhowmick",
+        "location" : "India",
+        "age" : 19
+    },
+    {
+        "id" : 14,
+        "name" : "Jeff-Kolomondor",
+        "location" : "Africa",
+        "age" : 25
+    },  
+    {
+        "id" : 15,
+        "name" : "Eddie-Joude",
+        "location" : "North-Amarica",
+        "age" : 25
+    },
+    {
+        "id" : 16,
+        "name" : "Kevin-Mctavish",
+        "location" : "UAE",
+        "age" : 22
+    },  
+    {
+        "id" : 17,
+        "name" : "Kim-Tas",
+        "location" : "China",
+        "age" : 39
+    },
+    {
+        "id" : 18,
+        "name" : "Trina-Chan",
+        "location" : "Singapore",
+        "age" : 21
+    },  
+    {
+        "id" : 19,
+        "name" : "Docso-Kolom",
+        "location" : "Africa",
+        "age" : 29
+    },
+    {
+        "id" : 20,
+        "name" : "Thomas-Bezos",
+        "location" : "Atlantica",
+        "age" : 22
+    },  
+    {
+        "id" : 21,
+        "name" : "Sandman-Pascal",
+        "location" : "UK",
+        "age" : 29
+    },
+    {
+        "id" : 22,
+        "name" : "Erik-Dojo",
+        "location" : "USA",
+        "age" : 25
+    },  
+]
+
+module.exports = users;
